a48a5ba17db569e5ee8f98df1f055e39d9aed85b
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2024-03-06 18:17:20 +0200

    Header and footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,24 @@
+import styles from "./Header.module.css";
+import Logo from "../../icons/logo.svg";
+import Phone from "../../icons/phone.svg";
+import { poppins } from "@/fonts/fonts";
+
 const Header = () => {
-  return <header>
-    Header
-  </header>;
+  return (
+    <header className={`${styles.header} ${poppins.className}`}>
+      <div className={styles.container}>
+        <Logo />
+        <nav className={styles.nav}>
+          <div className={styles.phone}>
+            <Phone />
+            <a href="tel:+4401618201101">01618201101</a>
+          </div>
+          <a href="#" className={styles.consultation}>Free consultation</a>
+          <a href="#" className={styles.licence}>Apply for a licence</a>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
